ebb589a27def6f79ec1ba07fdbf4114012a622b6
joshua.ribbons@my.jcu.edu.au <joshua.ribbons@my.jcu.edu.au@6ccaaa9d-41f0-1ec4-abe3-b00441ac8cc9>
2011-04-27 13:01:43 +0000

    Fixed a bug up that broke LINQ when querying dataset. Better demo page - dataGridView added with filter.

--- trunk/TestProject/Form1.Designer.cs
+++ trunk/TestProject/Form1.Designer.cs
@@ -30,35 +30,99 @@
         {
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.button1 = new System.Windows.Forms.Button();
+            this.artistTB = new System.Windows.Forms.TextBox();
+            this.titleTB = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.filterBtn = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(13, 13);
+            this.textBox1.Location = new System.Drawing.Point(1, 409);
             this.textBox1.Multiline = true;
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(734, 283);
+            this.textBox1.Size = new System.Drawing.Size(758, 24);
             this.textBox1.TabIndex = 0;
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(334, 354);
+            this.button1.Location = new System.Drawing.Point(1, -1);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.Size = new System.Drawing.Size(101, 23);
             this.button1.TabIndex = 1;
-            this.button1.Text = "button1";
+            this.button1.Text = "Load Database";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // artistTB
+            // 
+            this.artistTB.Location = new System.Drawing.Point(85, 28);
+            this.artistTB.Name = "artistTB";
+            this.artistTB.Size = new System.Drawing.Size(195, 20);
+            this.artistTB.TabIndex = 2;
+            // 
+            // titleTB
+            // 
+            this.titleTB.Location = new System.Drawing.Point(387, 28);
+            this.titleTB.Name = "titleTB";
+            this.titleTB.Size = new System.Drawing.Size(256, 20);
+            this.titleTB.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(9, 34);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(33, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Artist:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(300, 34);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(30, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Title:";
+            // 
+            // filterBtn
+            // 
+            this.filterBtn.Location = new System.Drawing.Point(678, 28);
+            this.filterBtn.Name = "filterBtn";
+            this.filterBtn.Size = new System.Drawing.Size(56, 19);
+            this.filterBtn.TabIndex = 6;
+            this.filterBtn.Text = "Filter";
+            this.filterBtn.UseVisualStyleBackColor = true;
+            this.filterBtn.Click += new System.EventHandler(this.filterBtn_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(8, 60);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(750, 349);
+            this.dataGridView1.TabIndex = 7;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(759, 433);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.filterBtn);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.titleTB);
+            this.Controls.Add(this.artistTB);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.textBox1);
             this.Name = "Form1";
             this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -68,6 +132,12 @@
 
         private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox artistTB;
+        private System.Windows.Forms.TextBox titleTB;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button filterBtn;
+        private System.Windows.Forms.DataGridView dataGridView1;
     }
 }
 
